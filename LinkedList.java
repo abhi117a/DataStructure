b4952114ea3beb02798db85f4bcc0fbd52f0c507
abhi117a <abhi_9trox@yahoo.co.uk>
2016-09-27 03:12:43 -0500

    Implementation of Linked List 1) Adding at the begining of List 2)Adding at the End of the list 3) Adding at the given location of the list
    Node1 is the Node Class, LinkedList is the class where methods are written

---
+++ LinkedList.java
@@ -0,0 +1,80 @@
+package CTC_2;
+
+public class LinkedList {
+	Node1 head;
+	public void addATBeginning(int data){
+		
+		if(head==null){
+			head  = new Node1();
+			head.data = data;
+		}
+		else{
+			Node1 p = new Node1();
+			p.data = data;
+			p.next = head;
+			head = p;
+		}
+	}
+	
+	public void addAtEnd(int data){
+		
+		if (head==null){
+			head = new Node1();
+			head.data = data;
+		}
+		
+		else {
+			Node1 p = head;
+			while(p.next!=null){
+			p = p.next;
+			}
+			p.next = new Node1();
+			p.next.data = data;
+			
+		}
+	}
+	
+	public void addatKnownLocation(int Loc,int data){
+		int count = Loc;
+		if(count==1){
+			Node1 p = new Node1() ;
+			p.data = data;
+			p.next = head;
+			head = p;
+		}
+		else if(count>1){
+			Node1 p = head;
+			while(p.next!=null&&count>2){
+				p = p.next;
+				count--;
+			}
+			Node1 temp = p.next;
+			p.next = new Node1();
+			p.next.data = data;
+			p.next.next = temp;
+			
+		}
+	}
+	public void printNodes(){
+		Node1 p =head;
+		while(p!=null){
+		System.out.println(p.data);
+		p = p.next;
+		}
+	}
+	
+	public static void main(String[] args) {
+		LinkedList ls = new LinkedList();
+		ls.addATBeginning(1);
+		ls.addATBeginning(2);
+		ls.addATBeginning(3);
+		ls.addATBeginning(4);
+		ls.addATBeginning(5);
+//		ls.addAtEnd(1);
+//		ls.addAtEnd(2);
+//		ls.addAtEnd(3);
+		ls.addatKnownLocation(1, 7);
+		ls.printNodes();
+	}
+
+}
---
+++ Node1.java
@@ -0,0 +1,7 @@
+package CTC_2;
+
+public class Node1 {
+	public Node1 next;
+	public int data;
+	
+}
