57d2b22f1beaa09df0a38fdff036a9213716941e
abhi117a <abhi_9trox@yahoo.co.uk>
2016-09-29 03:47:11 -0500

    Removing elements from linkedlist 1) At begining 2)At end 3)At given location by user

--- LinkedList.java
+++ LinkedList.java
@@ -63,6 +63,37 @@ public class LinkedList {
 		}
 	}
 	
+	public void removeAtBegining(){
+		Node1 p = head;
+		 head = p.next;
+	}
+	public void removeAtEnd(){
+		Node1 p = head;
+		while(p.next.next!=null){
+			p = p.next;
+		}
+		p.next=null;
+	}
+	public void removeAtLocation(int Loc){
+		int count = Loc;
+		
+		if(count==1){
+			Node1 p ;
+			 p = head;
+			head =p.next;
+		}
+		else if(count >1){
+			Node1 p =head;
+		while(p.next!=null && count > 3){
+			p = p.next;
+			count--;
+		}
+		p.next = p.next.next;
+		}
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		LinkedList ls = new LinkedList();
 		ls.addATBeginning(1);
@@ -70,10 +101,14 @@ public class LinkedList {
 		ls.addATBeginning(3);
 		ls.addATBeginning(4);
 		ls.addATBeginning(5);
+		ls.addATBeginning(6);
 //		ls.addAtEnd(1);
 //		ls.addAtEnd(2);
 //		ls.addAtEnd(3);
-		ls.addatKnownLocation(1, 7);
+		//ls.addatKnownLocation(1, 7);
+		ls.removeAtLocation(1);
+		//ls.removeAtEnd();
+		//ls.removeAtBegining();
 		ls.printNodes();
 	}
 
