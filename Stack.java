e982d4e9a5b8e5db8d5650f47ab5634bee295210
abhi117a <abhi_9trox@yahoo.co.uk>
2016-10-25 14:59:33 -0500

    Stack Practice create, push, pop, size and topelement of stack

---
+++ Stack.java
@@ -0,0 +1,67 @@
+package CTC_2;
+
+public class Stack {
+
+	int top;
+	int [] stack;
+	int capacity;
+	
+	public void create(int capacity){
+		this.capacity = capacity;
+		top=-1;
+		stack = new int[capacity];
+		for (int i=0;i<capacity;i++){
+			stack[i] = -1;
+		}
+	}
+	public void push(int val){
+		if(top+1 == capacity){
+			System.out.println("Stack is full");
+		}
+		else {
+			stack[++top] = val;
+		}
+	}
+	public void pop(){
+		if(top <0){
+			System.out.println("Stack is empty");
+		}
+		else{
+			stack[top]=-1;
+			System.out.println("Element Poped");
+			top--;
+		}
+	}
+	public void prnt(){
+		for(int i=top; i>=0; i--){
+			System.out.println(stack[i]);
+		}
+	}
+	
+	public void topStack(){
+		System.out.println("Top of Stack: "+ stack[top]);
+	}
+	public void sizeStack(){
+		System.out.println("Size of stack: "+ top);
+	}
+	
+	
+	public static void main(String[] args) {
+		Stack st = new Stack();
+		st.create(5);
+		st.push(10);
+		st.push(11);
+		st.push(12);
+		st.push(13);
+		st.push(14);
+		st.prnt();
+		//st.pop();
+		//st.pop();
+		st.pop();
+		st.pop();
+		st.pop();
+		System.out.println(" AFTER POP      ");
+		st.prnt();
+	}
+
+}
