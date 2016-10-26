a5d1b41238f21b714faba7b547d3df6acf3b4daf
abhi117a <abhi_9trox@yahoo.co.uk>
2016-10-26 15:30:54 -0500

    Implementing Queue using 2 stacks

---
+++ ImplementingQueue.java
@@ -0,0 +1,27 @@
+package CTC_2;
+import java.util.*;
+public class ImplementingQueue {
+	
+	public void stack1(){
+		int top =5;
+		int arr [] = new int[5];
+	arr[0] = 12;
+	arr[1] = 13;
+	arr[2] = 14;
+	arr[3] = 15;
+	arr[4]=  16;							
+	Stack st = new Stack();
+	Stack st1 = new Stack();
+	for(int i =0; i<arr.length;i++){
+	st.push(arr[i]);
+	}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
