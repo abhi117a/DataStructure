39fef2fc7bf1c00fe2bcce29e58096f7b3ced1d2
abhi117a <abhi_9trox@yahoo.co.uk>
2016-10-25 22:04:33 -0500

    Corrected The Code. Works in any case

--- minInStack.java
+++ minInStack.java
@@ -5,10 +5,12 @@ public class minInStack {
 	int minStack[];
 	int top =-1, topM =-1;
 	int size;
+	int minVal;
 	public void createStack(int size){
 		this.size = size;
 		mainStack = new int[size];
 		minStack = new int [size];
+		
 	}
 	public void push(int val){
 		if(top+1 == size){
@@ -18,25 +20,24 @@ public class minInStack {
 			mainStack[++top] = val;
 			if(topM == -1){
 				minStack[++topM] = val;
+				minVal = minStack[topM];
 			}
 			else if(minStack[topM] >= val){
 				minStack[++topM] = val;
+				minVal = val;
 			}
 			else {
-				minStack[++topM] = minStack[topM];
+				minStack[++topM] = minVal;
 			}
 		}
 	}
 	public void pop(){
-		int temp = minStack[0];
 	if(top <0){
 		System.out.println("Stack is empty");
-		//
 	}
 	else {
 		mainStack[top--] = -1;
 		minStack[topM--] = -1;
-		//System.out.println("Min Value is: "+minStack[topM]);
 	}
 	}
 	public void findMin(){
@@ -53,9 +54,12 @@ public class minInStack {
 	public static void main(String[] args) {
 		minInStack ms= new minInStack();
 		ms.createStack(5);
-		ms.push(5);
+		ms.push(1);
 		ms.push(2);
-		ms.push(9);
+		ms.push(3);
+		//ms.push(1);
+		ms.pop();
+		ms.pop();
 		ms.findMin();
 
 	}
