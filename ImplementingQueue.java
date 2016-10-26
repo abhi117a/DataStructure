d2399427bb6d84e0dc6141f6f4ee48733e15f8b3
abhi117a <abhi_9trox@yahoo.co.uk>
2016-10-26 16:14:44 -0500

    no message

--- ImplementingQueue.java
+++ ImplementingQueue.java
@@ -2,21 +2,28 @@ package CTC_2;
 import java.util.*;
 public class ImplementingQueue {
 	
-	public void stack1(){
-		int top =5;
-		int arr [] = new int[5];
-	arr[0] = 12;
-	arr[1] = 13;
-	arr[2] = 14;
-	arr[3] = 15;
-	arr[4]=  16;							
-	Stack st = new Stack();
-	Stack st1 = new Stack();
-	for(int i =0; i<arr.length;i++){
-	st.push(arr[i]);
+	java.util.Stack<Integer> s1 = new java.util.Stack<Integer>();
+	java.util.Stack<Integer> s2 = new java.util.Stack<Integer>();
+	
+	
+	public void push(Integer Obj){
+		s1.push(Obj);
 	}
-		
+	public Integer pop(){
+		if(s2.empty()){
+			if(s1.empty()){
+				return null;
+			}
+		}
+		else {
+			while(!s1.empty()){
+				s2.push(s1.pop());
+			}
+			 
+		}
+		return s2.pop();
 	}
+	
 	
 	
 	public static void main(String[] args) {
