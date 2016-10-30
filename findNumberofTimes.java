f115b17de1d0719fa532f2ecf9ce21143919560b
abhi117a <abhi_9trox@yahoo.co.uk>
2016-10-29 22:01:46 -0500

    Using HashMap in O(n) time complexity

--- findNumberofTimes.java
+++ findNumberofTimes.java
@@ -1,6 +1,10 @@
 package CTC_2;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 //If 2 values occur at same number of time it will give as last value as output! Time complexity O(n^2)
 public class findNumberofTimes {
 
@@ -24,11 +28,38 @@ public class findNumberofTimes {
 		System.out.println("Element with highest count is: "+find+" with number of occurence: "+finCount);
 	}
 	
+	public void hashFind(int A[]){
+//Using HashMap Time Complexity O(n) and Space Complexity O(n)
+		Map<Integer,Integer> hm = new HashMap<Integer,Integer>();
+		int count =0;
+		int key =0;
+		for(int i=0;i<A.length;i++){
+			if(hm.containsKey(A[i])){
+				int temp = (Integer)hm.get(A[i]);
+				hm.put(A[i],temp+1);
+			}
+			else {
+			hm.put(A[i], 1);	
+			}
+		}
+		 for (Entry<Integer,Integer> entry:hm.entrySet()){
+			 System.out.println(entry.getKey()+ "  "+entry.getValue());
+			 
+			  int val = entry.getValue();
+			 if(count < val){
+			 count = val;
+			 key = entry.getKey();
+			 }
+		 }
+		 System.out.println("Max repetation is "+count+" times and the value is "+key);
+	}
+	
 	
 	public static void main(String[] args) {
 		findNumberofTimes nt = new findNumberofTimes();
-		int a[] = {11,12,8,88,8,8,8,1,1,1,1,1,3,3,3,3,3};
+		int a[] = {11, 8, 8, 8, 3, 4, 1, 7};
 		nt.find(a);
+		nt.hashFind(a);
 
 	}
 
