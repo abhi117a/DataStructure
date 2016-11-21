ad73334eaaef8009ca2d428c6712d62296236e2a
abhi117a <abhi_9trox@yahoo.co.uk>
2016-11-20 23:05:29 -0600

    checking of the string if panagram

---
+++ checkIfPangram.java
@@ -0,0 +1,27 @@
+package CTC_2;
+
+public class checkIfPangram {
+
+	public void checkPangram(String a){
+		a.toLowerCase();
+		int CHAR[] = new int [26];
+		for(int i=0;i<a.length();i++){
+			char x =a.charAt(i);
+			CHAR[x-97]++;
+		}
+		for(int i: CHAR){
+		if(i ==0){
+			System.out.println("Not a panagram");
+			return;
+		}
+		}
+		System.out.println("Panagram it is");
+	}
+	
+	public static void main(String[] args) {
+		checkIfPangram cp = new checkIfPangram();
+		cp.checkPangram("abcdefghijklmnopqrstuvwxyz");
+
+	}
+
+}
