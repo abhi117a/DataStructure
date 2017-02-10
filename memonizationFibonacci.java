bd56323b798a7765e00242792856eb3bc55f1a4f
abhi117a <abhi_9trox@yahoo.co.uk>
2017-02-10 17:47:51 -0600

    Finding Fibonacci series for a number using memonization techqnique O(n)

---
+++ memonizationFibonacci.java
@@ -0,0 +1,31 @@
+package CTC_2;
+
+public class memonizationFibonacci {
+
+	memonizationFibonacci(int n){
+		int [] memo = new int [n+1];
+		for(int i=0;i<n;i++){
+			System.out.println(i+":" + fib(i,memo));
+		}
+	}
+	
+	private int fib(int n, int[] memo) {
+		if(n==0){
+			return 0;
+		}
+		else if(n==1){
+			return 1;
+		}
+		else if(memo[n]>0) {
+			return memo[n];
+		}
+		memo[n] = fib(n-1,memo) + fib(n-2,memo);
+		return memo[n];
+	}
+
+	public static void main(String[] args) {
+		memonizationFibonacci mz = new memonizationFibonacci(5);
+
+	}
+
+}
